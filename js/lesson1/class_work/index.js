237ba0828af81742b3c71273cca797f0ce7b1433
Yaroslav1724 <aroslavlukomskij@gmail.com>
2022-02-01 23:33:20 +0200

    add class work

---
+++ js/lesson1/class_work/index.js
@@ -0,0 +1,71 @@
+// Завданн 1
+
+let result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// Завдання 2 Створити об'єкт book
+const book = {
+    name: 'string',
+    page: 10,
+    genre: 'string'
+}
+
+// Завдання 3 Створити об'єкт book
+
+const new_book = {
+    name: 'string',
+    page: 20,
+    genre: 'string',
+    authors: ['string']
+}
+ // Завдання 4
+
+const books = [
+    {
+        name: 'Hobbit',
+        page: 400,
+        genre: 'fantasy',
+        authors: ['Peter Jackson']
+    },
+    {
+        name: "Hobbit_2",
+        page: 500,
+        genre: 'fantasy',
+        authors: ['Peter Jackson']
+    },
+    {
+        name: "Hobbit_3",
+        page: 600,
+        genre: 'fantasy',
+        authors: ['Peter Jackson']
+    }
+]
+
+console.log('Книга 1', books[0])
+console.log('Книга 2', books[1])
+console.log('Книга 3', books[2])
+
+// Завдання 5
+
+const height = 23;
+const widht = 10;
+const s = height * widht;
+console.log('площа прямокутника', s)
+
+// Завдання 6
+
+const heightC = 10;
+const dC = 4;
+const PI = 3.14;
+const rC = dC/2;
+const v = PI * rC * rC * heightC;
+console.log('обєм циліндра', v)
+
+// Завдання 7
+
+const n = 3;
+const m = 4;
+const k = Math.pow(n**2 + m**2, 0.5)
+console.log('гіпотенуза прямокутного трикутника', k)
+
+
+
