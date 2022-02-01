a82784c569b8eede0717c4fa463f8842b6101813
Yaroslav1724 <aroslavlukomskij@gmail.com>
2022-02-01 22:26:43 +0200

    add home_work

---
+++ js/lesson1/home_work/index.js
@@ -0,0 +1,101 @@
+// Завдання 1 створення змінних
+
+let wortOne = 'hello';
+let one = `owu`;
+let wort = `com`;
+let two = `ua`;
+let sims = 11;
+let id = -999;
+let car = 123;
+let pi = 3.14;
+let clas = 10;
+let bart =  2.7;
+let box_1 = 16;
+let box_one = true;
+let sii = false
+console.log(wortOne, one, wort, two, sims, bart, id, car, pi, clas, box_1, box_one, sii)
+document.write(wortOne, one, wort, two, sims, bart, id, car, pi, clas, box_1, box_one, sii)
+alert(wortOne)
+alert(one)
+alert(wort)
+alert(two)
+alert(sims)
+alert(bart)
+alert(id)
+alert(car)
+alert(pi)
+alert(clas)
+alert(box_1)
+alert(box_one)
+alert(sii)
+
+// Завдання 2 Переприсвоїти кожній змінній з завдання значення на довільне.
+
+wortOne = 'contact'
+one = 'cons'
+wort = 'six'
+two = 'text'
+sims = 22
+bart = 33
+id = 44
+car = 55
+pi = 66
+clas = 77
+box_1 = 88
+box_one = 'cat'
+sii = 'black'
+console.log(wortOne, one, wort, two, sims, bart, id, car, pi, clas, box_1, box_one, sii)
+document.write(wortOne, one, wort, two, sims, bart, id, car, pi, clas, box_1, box_one, sii)
+alert(wortOne)
+alert(one)
+alert(wort)
+alert(two)
+alert(sims)
+alert(bart)
+alert(id)
+alert(car)
+alert(pi)
+alert(clas)
+alert(box_1)
+alert(box_one)
+alert(sii)
+
+// Завдання 3 Створити 3 змінних
+
+let firstName = 'Yaroslav';
+let middleName = 'Orestovich';
+let lastName = 'Lukomskyi';
+let name = firstName + ' ' + middleName + ' ' + lastName;
+console.log(name)
+
+// Завдання 4  За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+
+let cons = prompt('введіть ваше імя')
+let text = prompt('вік')
+let box = prompt('по батькові')
+let can = `вітаю ${cons} ${box}. тобі ${text} років`;
+console.log(can)
+
+// Завдання 5 За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+
+let a = 100; let b = '100'; let c = true;
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+// Завдання 6 Визначити відповідний оператор в виразах що б вийшов відповідний результат
+
+// >  <  <=  >=  ==  ===  != !==
+
+console.log(5 < 6)
+console.log(5 > 6)
+console.log(5 >= 6)
+console.log(5 == 6)
+console.log(10 === 10)
+console.log(10 <= 10)
+console.log(10 != 10)
+console.log(10 !== 10)
+console.log(10 < 10)
+console.log(123 > 123)
+console.log(123 === 123)
+
