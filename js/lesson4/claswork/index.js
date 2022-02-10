c472cdf7fb6d93831ef9b067863308e42251acd9
Yaroslav1724 <aroslavlukomskij@gmail.com>
2022-02-10 21:51:16 +0200

    add class work homework

--- js/lesson4/claswork/index.js
+++ js/lesson4/claswork/index.js
@@ -1,8 +1,148 @@
 
   // Завдання 1 Створити функцію яка приймає три числа та виводить найменше. (Без Math.min!)
+//
+//   function num1 (a, b, c) {
+//     if (a < b && a < c){
+//         document.write(a);
+//         console.log(a);
+//     }
+//     else if (a < c && b < a){
+//         console.log(b);
+//         document.write(b);
+//     }
+//     else {
+//         console.log(c);
+//         document.write(c);
+//     }
+// }
+// num1(1,2,3)
 
-  function num1 (one, two, three) {
-    if (one < two && one <three)
-        document.write(one)
+  // Завдання 2 Створити функцію яка приймає три числа та виводить найбільше
+//
+//   function num2 (a, b, c) {
+//       if(a > b && a > c){
+//           console.log(a)
+//           document.write(a)
+//       }
+//       else if(b > a && b > c){
+//           console.log(b)
+//           document.write(b)
+//       }
+//       else {
+//           console.log(c)
+//           document.write(c)
+//       }
+//   }
+// num2(4,5,6)
 
-  }
+  // Завдання 3 Створити функцію яка повертає найбільше число з масиву
+ //
+ //  let maxarr = [2, 12, 432, 3124, 3244]
+ //  function arrmax(arr){
+ //      let max = arr[0];
+ //      for (let element of arr) {
+ //          if(element > max){
+ //              max = element
+ //          }
+ //      }
+ //      return max;
+ // }
+ // document.write(`${arrmax(maxarr)}`)
+
+  // Завдання 4 - Створити функцію яка повертає найменьше число з масиву
+ //
+ //  let minarr = [11, 22, 33]
+ //  function minnum(arr){
+ //    let min = arr[0]
+ //      for (let element of arr) {
+ //          if (element < min){
+ //              min = element;
+ //          }
+ //      }
+ //      return min;
+ //  }
+ // document.write(`${minnum(minarr)}`)
+
+  //   Завдання 5 - Створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+//
+//
+//
+//   let sumarr = [2, 4, 6, 10];
+//   function sumElement(arr) {
+//     let sum = 0;
+//     for (let arrElement of arr) {
+//         sum = arrElement + sum
+//     }
+//     return sum;
+//   }
+// document.write(sumElement(sumarr))
+
+  //Завдання 6 - Cтворити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
+  //
+  // let averagearr = [2, 4, 9,]
+  // function averageElement(arr){
+  //     let sum = 0;
+  //     for (let arrElement of arr) {
+  //         sum = arrElement + sum;
+  //     }
+  //     return sum/arr.length;
+  // }
+  // document.write(averageElement(averagearr))
+  //
+
+  // Завдання 7 - Створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+  //              0
+  //let array = [100,233,111]
+  //    function minmax(...arr){
+  //        let min = arr[0];
+  //         let max = arr[0];
+  //          for (let element of arr) {
+  //       if(element > max){
+  //          max = element;
+  //       }
+  //        if(element < min){
+  //            min = element;
+  //        }
+  //          }
+  //      console.log(max)
+  //      return min;
+  // }
+  // document.write(minmax(100, 233, 111))
+
+  // Завдання 8 - Створити функцію яка заповнює масив рандомними числами
+  //
+  // function funrandom (length){
+  // let array = []
+  //   for (let i = 0; i <length; i++) {
+  //     array.push(Math.floor(Math.random()*100));
+  //
+  //   }
+  //   return array;
+  // }
+  // document.write(funrandom(10))
+
+  // Завдання 9 - Створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+  //
+  //
+  // function randomnum(length, limit){
+  // let array = [];
+  //   for (let i = 0; i < length; i++) {
+  //     array.push(Math.round(Math.random() * limit));
+  //
+  //   }
+  //   return array;
+  // }
+  // document.write(randomnum(10,100))
+
+  // Завдання 10 - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+  // debager
+  //
+  // function revers(arr){
+  //    let newarr = [];
+  //   for (let i =  arr.length -1,ri=0; i >= 0; i--, ri++) {
+  //     newarr[ri] = arr[i];
+  //   }
+  //   return newarr;
+  // }
+  // revers([1,2,3])
+  // console.log(revers([1, 2, 3]))
---
+++ js/lesson4/homework/index.js
@@ -0,0 +1,95 @@
+
+    // Завдання 1 - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+    //
+    // function square (a, b){
+    //  Ssquare = a * b;
+    // return Ssquare;
+    // }
+    // console.log(square(10, 10))
+    // document.write(square(10, 10))
+
+    // Завдання 2 - Створити функцію яка обчислює та повертає площу кола з радіусом r
+//
+//     function scircle(r){
+//     let circle = 3.14 * r;
+//     return circle;
+// }
+//
+//     console.log(scircle(10))
+//     document.write(scircle(10))
+
+    // Завдання 3 - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+    //
+    // function cylinder(h,r){
+    // let scylinder = 2 *3.14 * r * h;
+    // return scylinder;
+    // }
+    //
+    // console.log(cylinder(10, 10))
+    // document.write(cylinder(11, 11))
+
+    // Завдання 4 - Створити функцію яка приймає масив та виводить кожен його елемент
+    //
+    //  let arr = [11, 22, 33, 'asd']
+    // function array(arr){
+    //     for (let i = 0; i < arr.length; i++) {
+    //         document.write(arr[i])
+    //
+    //     }
+    // }
+    // array(arr)
+    //
+    // Завдання 5 - Створити функцію яка створює параграф з текстом. Текст задати через аргумент
+
+    //
+  //   function paragraph(text){
+  //       for (let i = 0; i <= 10; i++){
+  //           document.write(`<p>${i} - ${text}</p>`)
+  //       }
+  //   }
+  // paragraph('empty text')
+
+    // Завдання 6 - Створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+    //
+    //
+    // function txt(text){
+    //     document.write(`<ul>`)
+    //     document.write(`<li>${text}</li>`)
+    //     document.write(`<li>${text}</li>`)
+    //     document.write(`<li>${text}</li>`)
+    //     document.write(`</ul>`)
+    // }
+    // txt('hello')
+
+    // Завдання 7 - Створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+    //
+    // function txt(text, num) {
+    //       document.write(`<ul>`)
+    //     for (let i = 0; i < num; i++) {
+    //         document.write(`<li>${text}</li>`);
+    //     }
+    //      document.write(`</ul>`)
+    //
+    // }
+    // txt('hello', '30' )
+
+    // Завдання 8 - Створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+    //
+    // let asd = [true,'stop', 2012]
+    // function arr(list){
+    //     for (let i = 0; i < list.length; i++) {
+    //         document.write(`<li>${list[i]}</li>`);
+    //     }
+    // }
+    // arr(asd);
+
+    // Завдання 9 - Створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+    //
+    // let obj = [{id: 1, name: 'ivan', age: 88}, {id: 2, name: 'anna', age: 92},{id: 3, name: 'taras', age: 10}];
+    // function family(asd){
+    //     for (let qweElement of asd) {
+    //         document.write(`<div>${qweElement.id} - ${qweElement.name} - ${qweElement.age}</div>`)
+    //
+    //     }
+    // }
+    // family(obj)
